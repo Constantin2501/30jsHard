5f83ad0418c23217488bab35b3f3824caf57f345
Constantine <ckwebmast@gmail.com>
2023-09-26 17:51:41 +0300

    comlpete lesson05

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const getSting = (str) => {
 
-    if (typeof str !== 'string') {
-        console.log('передана не строка')
-        return
+let n = 100
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) continue nextPrime
     }
 
-    str = str.trim()
-
-    if (str.length > 30) {
-        str = str.substring(0, 30) + '...'
-    }
-
-    return str
+    console.log(i + ' ' + `Делители этого числа: 1 и ${i}`)
 }
 
-console.log(getSting('Готово'))
+
+
+const arr = ['234124', '523235', '4424142', '42143151', '124342', '124532125', '652351']
+
+arr.forEach( (e) => {
+    if (e.startsWith('2') || e.startsWith('4')) {
+        console.log(e)
+    }
+})
+
+
+
+let screenPrice
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+const asking = function () {
+
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?')
+        if (screenPrice !== null) {
+            screenPrice = screenPrice.trim()
+        }
+    } while (!isNumber(screenPrice))
+
+}
+asking()
+console.log(typeof screenPrice)
+console.log(screenPrice)
