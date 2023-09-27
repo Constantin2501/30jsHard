02584d1a501326a3cd6d2d0719c26172eca182de
Constantine <ckwebmast@gmail.com>
2023-09-27 22:37:41 +0300

    feat: hard functional

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 const guessNumBot = function () {
+    let tryCount = 10
 
     const randomNum = function () {
         return Math.round(Math.random() * 99 + 1)
     }
 
     const successNum = function () {
-        let userNum = prompt('Угадай число от 1 до 100')
 
-        if (isNaN(userNum) || userNum === '') {
-            alert('Введи число!')
-            successNum()
+        if (tryCount === 0) {
+            let tryAgain = confirm('Попытки закончились, хотите сыграть еще?')
+            if (tryAgain) {
+                guessNumBot()
+            } else alert('спасибо за игру!')
+            return
         }
+
+        let userNum = prompt(`Угадай число от 1 до 100. Осталось попыток: ${tryCount}`)
 
         if (userNum === null) {
             alert('Игра окончена')
             return
         }
-        
-        if (userNum === numBot) {
-            alert('Поздравляю, Вы угадали!!!')
+
+        if (+userNum === numBot) {
+            let playAgain = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?')
+            if (playAgain) {
+                guessNumBot()
+            } else alert('спасибо за игру!')
             return
         }
 
+        if (isNaN(+userNum) || userNum === '') {
+            alert('Введи число!')
+            successNum()
+        }
+
         if (userNum > numBot) {
-            alert('Загаданное число меньше')
+            tryCount--
+            alert(`Загаданное число меньше. Осталось попыток: ${tryCount}`)
             successNum()
-        } 
-        
+        }
+
         if (userNum < numBot) {
-            alert('Загаданное число больше')
+            tryCount--
+            alert(`Загаданное число больше Осталось попыток: ${tryCount}`)
             successNum()
-        } 
+        }
+
 
     }
     
